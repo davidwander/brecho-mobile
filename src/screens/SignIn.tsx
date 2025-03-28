2f4b519d5e4043c9b3bf0c433aeb2768920c28e4
David <dwander616@gmail.com>
2025-03-27 21:50:31 -0300

    Finalizing SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Center, Heading, Text } from '@gluestack-ui/themed';
+import { VStack, Image, Center, Heading, Text, ScrollView } from '@gluestack-ui/themed';
 
 import BackgroundImg from '@assets/background.png';
 import Logo from '@assets/vb-logo.png';
@@ -8,59 +8,64 @@ import { Button } from '@components/Button';
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="$textDark800">
-      <Image 
-        w="$full"
-        h={624}
-        source={BackgroundImg} 
-        defaultSource={BackgroundImg}
-        alt="Imagem de fundo" 
-        position="absolute"
-      />
+    <ScrollView 
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+      >
+      <VStack flex={1} bg="$textDark800">
+        <Image 
+          w="$full"
+          h={624}
+          source={BackgroundImg} 
+          defaultSource={BackgroundImg}
+          alt="Imagem de fundo" 
+          position="absolute"
+        />
 
-      <VStack flex={1} px="$10" pb="$16">
-        <Center my="$24" >
-          <Image 
-            source={Logo} 
-            rounded="$full" 
-            alt="Logotipo brecho Vitoriano" 
-          />
+        <VStack flex={1} px="$10" pb="$16">
+          <Center my="$24" >
+            <Image 
+              source={Logo} 
+              rounded="$full" 
+              alt="Logotipo brecho Vitoriano" 
+            />
 
-          <Text color="$purple500" fontSize="$sm" lineHeight="$xl">
-            App Gerenciador E-commerce
-          </Text>
-        </Center>
+            <Text color="$purple500" fontSize="$sm" lineHeight="$xl">
+              App Gerenciador E-commerce
+            </Text>
+          </Center>
 
-        <Center gap="$2">
-          <Heading color="$textLight400">
-            Entrar em Vitoriano Brecho
-          </Heading>
+          <Center gap="$2">
+            <Heading color="$textLight400">
+              Entrar em Vitoriano Brecho
+            </Heading>
 
-          <Input 
-            placeholder="E-mail" 
-            keyboardType="email-address" 
-            autoCapitalize="none" 
-          />
+            <Input 
+              placeholder="E-mail" 
+              keyboardType="email-address" 
+              autoCapitalize="none" 
+            />
 
-          <Input placeholder="Senha" secureTextEntry />
+            <Input placeholder="Senha" secureTextEntry />
 
-          <Button title="Entrar" />
-        </Center>
+            <Button title="Entrar" />
+          </Center>
 
-        <Center flex={1} justifyContent="flex-end" mt="$4">
-          <Text
-            color="$textLight400"
-            mb="$3"
-            fontFamily="$body"
-            fontSize="$sm"
-            lineHeight="$xl"
-          >
-            Ainda não tem acesso?
-          </Text>
+          <Center flex={1} justifyContent="flex-end" mt="$4">
+            <Text
+              color="$textLight400"
+              mb="$3"
+              fontFamily="$body"
+              fontSize="$sm"
+              lineHeight="$xl"
+            >
+              Ainda não tem acesso?
+            </Text>
 
-          <Button title="Criar conta" variant="outline" />
-        </Center>
+            <Button title="Criar conta" variant="outline" />
+          </Center>
+        </VStack>
       </VStack>
-    </VStack>
+    </ScrollView>
   )
 }
