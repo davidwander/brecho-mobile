80bc50ca6f61dcbcb1901bc218f6c2581e5587a4
David <dwander616@gmail.com>
2025-03-28 10:10:39 -0300

    modifying navigation theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,20 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { Box } from '@gluestack-ui/themed';
 
 import { AuthRoutes } from './auth.routes';
 
+import { gluestackUIConfig } from '@gluestack-ui/config';
+
+
 export function Routes() {
+  const theme = DefaultTheme
+  theme.colors.background = gluestackUIConfig.tokens.colors.textDark800;
+
   return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="$textDark800">
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
   )
 }
