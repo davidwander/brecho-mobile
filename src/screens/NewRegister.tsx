d7d2f883fac69ab5d429b420f3358131697f5f1d
David <dwander616@gmail.com>
2025-04-23 18:44:33 -0300

    Adding handleSubmit to the calculate sale price button

--- src/screens/NewRegister.tsx
+++ src/screens/NewRegister.tsx
@@ -244,7 +244,7 @@ export function NewRegister() {
 
               <Button 
                 title="Calcular valor de venda"
-                onPress={calculateSalePrice} 
+                onPress={handleSubmit(() => calculateSalePrice())} 
                 isLoading={isCalculating}
               />
               <Input 
