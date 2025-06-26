a7e528a47b05fc36ad6fcd3ed0066ac190a3a5fe
David <dwander616@gmail.com>
2025-06-26 19:35:56 -0300

    refactor(app): improve providers structure

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
   Roboto_400Regular,
 } from '@expo-google-fonts/roboto';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { NavigationContainer } from '@react-navigation/native';
 
 import { config } from './config/gluestack-ui.config';
 
@@ -17,27 +18,29 @@ import { DeliveryProvider } from '@contexts/DeliveryContext';
 import { ClientProvider } from '@contexts/ClientContext';
 
 export default function App() {
-  const [fontsLoaded] = useFonts ({ Roboto_700Bold, Roboto_400Regular });
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
 
   return (
-    <GluestackUIProvider config={config}>
-      <AuthProvider>
-        <ProductProvider>
-          <SalesProvider>
-            <DeliveryProvider>
-              <ClientProvider>
-                <StatusBar 
-                  barStyle="light-content" 
-                  backgroundColor="transparent" 
-                  translucent
-                />
+    <NavigationContainer>
+      <GluestackUIProvider config={config}>
+        <AuthProvider>
+          <ProductProvider>
+            <SalesProvider>
+              <DeliveryProvider>
+                <ClientProvider>
+                  <StatusBar 
+                    barStyle="light-content" 
+                    backgroundColor="transparent" 
+                    translucent
+                  />
 
-                {fontsLoaded ? <Routes /> : <Loading /> }
-              </ClientProvider>
-            </DeliveryProvider>
-          </SalesProvider>
-        </ProductProvider>
-      </AuthProvider>
-    </GluestackUIProvider>
+                  {fontsLoaded ? <Routes /> : <Loading />}
+                </ClientProvider>
+              </DeliveryProvider>
+            </SalesProvider>
+          </ProductProvider>
+        </AuthProvider>
+      </GluestackUIProvider>
+    </NavigationContainer>
   );
 }
